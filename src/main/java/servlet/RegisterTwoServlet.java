f2d2608039b6b9aa07c2b4ea33aeb177e15cf7a8
Juan E Daza <juandape@unisabana.edu.co>
2021-04-30 20:29:34 -0500

    d

--- src/main/java/servlet/RegisterTwoServlet.java
+++ src/main/java/servlet/RegisterTwoServlet.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet(name = "RegisterTwoServlet", urlPatterns = { "/registertwo" })
 public class RegisterTwoServlet extends HttpServlet {
 
     @Override
