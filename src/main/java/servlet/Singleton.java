610984532230cb60fca20e89bd8f3aa1373a4988
Juan E Daza <juandape@unisabana.edu.co>
2021-05-02 16:11:46 -0500

    Co-authored-by: juan2409 <juan2409@users.noreply.github.com>

--- src/main/java/servlet/Singleton.java
+++ src/main/java/servlet/Singleton.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javassist.compiler.ast.StringL;
+
 /*
 CREATE TABLE MEDICO (
     id_medico VARCHAR(10) NOT NULL,
@@ -79,28 +81,28 @@ public class Singleton {
     private static Singleton instancia;
 
     private static Connection connSQL = null;
-    private static PreparedStatement statement = null;
 
-        //LocalHost
-    /*private static String userBD = "root";
-    private static String passBD = "1234";
-    private static String urlBD = "jdbc:mysql://localhost:3306/prueba_local";
-    */
+    // LocalHost
+    /*
+     * private static String userBD = "root"; private static String passBD = "1234";
+     * private static String urlBD = "jdbc:mysql://localhost:3306/prueba_local";
+     */
 
-    //Heroku
+    // Heroku
 
     private static String userBD = "be0469cbf4e9eb";
     private static String passBD = "cdb0227e";
     private static String urlBD = "jdbc:mysql://us-cdbr-east-03.cleardb.com/heroku_fdfec516ca01a53";
 
-    //mysql://be0469cbf4e9eb:cdb0227e@us-cdbr-east-03.cleardb.com/heroku_fdfec516ca01a53?reconnect=true
+    // mysql://be0469cbf4e9eb:cdb0227e@us-cdbr-east-03.cleardb.com/heroku_fdfec516ca01a53?reconnect=true
 
     private static int medidini = 85632;
+    private static int pacceidini = 4120;
 
-    static Singleton getSingleton(){
-        if(instancia == null){
-            synchronized(Singleton.class){
-                if(instancia == null){
+    static Singleton getSingleton() {
+        if (instancia == null) {
+            synchronized (Singleton.class) {
+                if (instancia == null) {
                     instancia = new Singleton();
                 }
             }
@@ -108,91 +110,128 @@ public class Singleton {
 
         return instancia;
     }
-    
-    public static void connectarBD(){
+
+    public static void connectarBD() {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("se ha encontrado el driver");
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
             e.printStackTrace();
             System.out.println("No se ha encontrado el driver");
         }
 
         try {
             connSQL = DriverManager.getConnection(urlBD, userBD, passBD);
-            
-            if(connSQL != null){
+
+            if (connSQL != null) {
                 System.out.println("Se ha logrado la conexión con la BD");
-            }else{
+            } else {
                 System.out.println("No se ha logrado la conexión con la BD");
             }
         } catch (SQLException e) {
-            //TODO: handle exception
+            // TODO: handle exception
             e.printStackTrace();
             System.out.println("Conexión fallida");
         }
     }
 
-    public static void traerData(){
+    public static void traerData() {
 
         try {
             String queryTraer = "SELECT * FROM usuarios";
+
+            PreparedStatement statement = null;
 
             statement = connSQL.prepareStatement(queryTraer);
 
             ResultSet rs = statement.executeQuery();
 
-            while(rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String correo = rs.getString("correo");
                 String contra = rs.getString("contrasegna");
 
                 System.out.println(id + ", " + correo + ", " + contra);
             }
-            
+
         } catch (SQLException e) {
-            //TODO: handle exception
+            // TODO: handle exception
             e.printStackTrace();
         }
     }
 
-    public static void insertData(String correo, String password){
+    public static void insertData(String correo, String password) {
 
         try {
             String queryInsertar = "INSERT INTO usuarios (correo, contrasegna) VALUES (?,?)";
+
+            PreparedStatement statement = null;
 
             statement = connSQL.prepareStatement(queryInsertar);
             statement.setString(1, correo);
             statement.setString(2, password);
 
-
             statement.executeUpdate();
             System.out.println(correo + " Se ha añadido correctamente");
         } catch (SQLException e) {
-            //TODO: handle exception
+            // TODO: handle exception
             e.printStackTrace();
         }
     }
 
-    public static void cerrarConexion(){
+    public static void cerrarConexion() {
         try {
             connSQL.close();
         } catch (SQLException e) {
-            //TODO: handle exception
+            // TODO: handle exception
         }
     }
 
-    public static String crearIdMedico(){
-        medidini++; 
-        String idm = "M"+medidini;
+    public static String crearIdMedico() {
+        medidini++;
+        String idm = "M" + medidini;
         return idm;
     }
 
-    public static String getPacientesMed(){
+    public static int crearIdContactoEmer() {
+        pacceidini++;
+        return pacceidini;
+    }
+
+    public static void ingresarPaciente(int id_p, String correoP, String generoP, int edadP, String nombreP,
+            String telefonoP, String sangreP, String estadoP, int idCE, String parentesoCE, String telefonoCE,
+            int edadCE, String correoCE, String nombreCE) {
+
+        
+
+    }
+
+    public static String getPacientesMed() {
 
         String str = "";
+
+        try {
+            String queryPacienteEstado = "";
+            PreparedStatement statement = null;
+
+            statement = connSQL.prepareStatement(queryPacienteEstado);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre_paciente");
+                String estado = rs.getString("estado_paciente");
+
+                str += nombre + ", " + estado;
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
         return str;
 
     }
